77f4c283b66471f8059d96558f3f48282d5e8aa5
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-28 21:06:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -53,10 +53,10 @@ def move(board, index, character = "X")
   return board
 end
 
-def turn(board)
+def turn(board, index)
   puts "Please enter 1-9:"
   user_input = gets.strip.to_i
   input_to_index(user_input)
-  
+  index = converted_input
 
 end
